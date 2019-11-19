b6dd053aa9d834563e5654d5b1d55d0dbfe4632f
SaurabhDesh <saurabh@spicetech.in>
2019-11-19 19:44:11 +0530

    added requirements.txt, Procfile

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import ImageFile
 from PIL import Image
 from flask import Flask, request, jsonify
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = '../Automated-Exam-Evaluation/scripts/Tesseractengine/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 app = Flask(__name__)
